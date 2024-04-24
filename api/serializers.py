d5f5c4860ac65e6d544e5058aad5fe9265046b35
Ajay Crest <ajay.g@crestinfosystems.com>
2024-04-24 15:44:18 +0530

    EntrySerializers issue fixed

--- api/serializers.py
+++ api/serializers.py
@@ -9,15 +9,20 @@ class PartySerializers(serializers.ModelSerializer):
 
 class EntrySerializers(serializers.ModelSerializer):
     # party = serializers.PrimaryKeyRelatedField(queryset=PartyModel.objects.all())
-
+    party = serializers.SerializerMethodField()
     class Meta:
         model = EntryModel
         fields = "__all__"
+    
+    def get_party(self,obj):
+        print(obj.party)
+        return {"party_name":obj.party.party_name,"party_id":obj.party.party_id}
+    
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        print(representation)
-        party_id = representation['party']
-        party_name = PartyModel.objects.get(party_id=party_id).party_name
-        representation['party'] = {'party_id': party_id, 'party_name': party_name}
-        return representation
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     print(representation)
+    #     party_id = representation['party']
+    #     party_name = PartyModel.objects.get(party_id=party_id).party_name
+    #     representation['party'] = {'party_id': party_id, 'party_name': party_name}
+    #     return representation
